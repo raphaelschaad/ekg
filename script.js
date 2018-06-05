474dd78b133ae538bba1bf0c9b269267d6785cf0
Raphael Schaad <raphael.schaad@gmail.com>
2018-06-06 00:48:22 +0200

    Show diagnosis text over EKG, not below video
    Also make type bigger and green

--- script.js
+++ script.js
@@ -1,13 +1,14 @@
-function show(videoId) {
-  let video = document.getElementById(videoId);
-  for (let element of video.children) {
+function show(id) {
+  let elements = document.getElementsByClassName(id);
+  console.log(elements);
+  for (let element of elements) {
     element.style.visibility = 'visible';
   }
 }
 
-function hide(videoId) {
-  let video = document.getElementById(videoId);
-  for (let element of video.children) {
+function hide(id) {
+  let elements = document.getElementsByClassName(id);
+  for (let element of elements) {
     element.style.visibility = 'hidden';
   }
 }
