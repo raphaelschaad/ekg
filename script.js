32e34a79853611d9433e82ea71bd7553a81dc8dd
Raphael Schaad <raphael.schaad@gmail.com>
2018-06-06 10:11:11 +0200

    Show/hide interactive 3D models onclick so one can orbit them around

--- script.js
+++ script.js
@@ -1,14 +1,36 @@
+function showElement(element) {
+  element.style.visibility = 'visible';
+}
+
+function hideElement(element) {
+  element.style.visibility = 'hidden';
+}
+
 function show(id) {
-  let elements = document.getElementsByClassName(id);
+  const elements = document.getElementsByClassName(id);
   console.log(elements);
   for (let element of elements) {
-    element.style.visibility = 'visible';
+    showElement(element);
   }
 }
 
 function hide(id) {
-  let elements = document.getElementsByClassName(id);
+  const elements = document.getElementsByClassName(id);
   for (let element of elements) {
-    element.style.visibility = 'hidden';
+    hideElement(element);
+  }
+}
+
+function toggleVisibility(id) {
+  const elements = document.getElementsByClassName(id);
+  for (let element of elements) {
+    if (element.style.visibility === 'visible') {
+      hideElement(element);
+    } else if (element.style.visibility === 'hidden') {
+      showElement(element);
+    } else {
+      console.log('Unknown visibility for element:');
+      console.log(element);
+    }
   }
 }
